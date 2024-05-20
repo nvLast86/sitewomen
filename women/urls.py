40c70a1c3c38ff0cf3fdf33f2814b72e65196dbe
nvLast86 <n.lastenko@gmail.com>
2024-05-20 14:32:04 +0600

    Tasks 1.4-1.6 has been completed

---
+++ women/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, register_converter
+from . import views, converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
+
+
+urlpatterns = [
+    path('', views.index),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('archive/<year4:year>', views.archive),
+]
--- women/views.py
+++ women/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse('Страница приложения women')
+
+
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
