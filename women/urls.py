4937ace6b7f175693d4cec15a91cc3c64784b933
nvLast86 <n.lastenko@gmail.com>
2024-05-27 11:11:01 +0600

    Tasks 2.1-2.2 have been completed

--- women/urls.py
+++ women/urls.py
@@ -6,6 +6,7 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index),
+    path('about/', views.about),
     path('cats/<int:cat_id>/', views.categories),
     path('cats/<slug:cat_slug>/', views.categories_by_slug),
     path('archive/<year4:year>', views.archive),
