4c39222fd8a5d9dbdca3d435a197935e517498f5
nvLast86 <n.lastenko@gmail.com>
2024-07-04 19:19:43 +0600

    task 2.8 has been completed

--- women/views.py
+++ women/views.py
@@ -18,12 +18,19 @@ data_db = [
             {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
             ]
 
+cats_db = [
+    {'id': 1, 'name': 'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
+]
+
 
 def index(request):
     data = {
         'title': 'главная страница',
         'menu': menu,
         'posts': data_db,
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
 
@@ -35,6 +42,16 @@ def about(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_categories(request, cat_id):
+    data = {
+        'title': 'отображение по рубрикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 
 def addpage(request):
