ed9e29613e6afd49045d1ffa15dd3c80398a44bc
user <n.lastenko@gmail.com>
2024-07-07 09:15:40 +0600

    task 3.7 and Module 3 have been completed

--- women/views.py
+++ women/views.py
@@ -28,7 +28,7 @@ cats_db = [
 
 
 def index(request):
-    posts = Women.objects.filter(is_published=1)
+    posts = Women.published.all()
     data = {
         'title': 'главная страница',
         'menu': menu,
