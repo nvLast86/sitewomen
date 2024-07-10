c015d125225e52ee2bb6ee7fadea39b9de086aa1
user <n.lastenko@gmail.com>
2024-07-10 07:36:59 +0600

    task 4.5 has been completed

--- women/models.py
+++ women/models.py
@@ -19,6 +19,7 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT,)
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -47,6 +48,14 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse ('category', kwargs={'cat_slug': self.slug})
 
+
+class TagPost(models.Model):
+
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
 
 
 
