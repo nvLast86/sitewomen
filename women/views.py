97e8b2019994e5f59e2103145f9ea70cae00fc83
user <n.lastenko@gmail.com>
2024-07-12 00:03:01 +0600

    task 4.6 has been completed

--- women/views.py
+++ women/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.urls import reverse
 from django.template.defaultfilters import slugify
 
-from women.models import Women, Category
+from women.models import Women, Category, TagPost
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -61,6 +61,17 @@ def show_categories(request, cat_slug):
     }
     return render(request, 'women/index.html', context=data)
 
+
+def show_tag_postlist(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug=tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+    data = {
+        'title': f'Тег: {tag.tag}',
+        'menu': menu,
+        'posts': posts,
+        'cat_selected': None,
+    }
+    return render(request, 'women/index.html', context=data)
 
 def addpage(request):
     return HttpResponse('Добавление статьи')
