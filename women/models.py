bf24ace1009f63173b34c95d9848090e3d6fb631
nvLast86 <n.lastenko@gmail.com>
2024-07-23 08:09:31 +0600

    task 6.5 has been completed

--- women/models.py
+++ women/models.py
@@ -41,6 +41,10 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
+    # def save(self, *args, **kwargs):
+    #    self.slug = slugify(self.title)
+    #    super().save(*args, **kwargs)
+
 
 class Category(models.Model):
 
