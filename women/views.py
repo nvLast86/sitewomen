cb692f605db3c787ce39d4028fd16dbf62de5c9b
nvLast86 <n.lastenko@gmail.com>
2024-07-29 14:26:17 +0600

    task 7.3 has been completed

--- women/views.py
+++ women/views.py
@@ -72,13 +72,18 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, "Ошибка добавления поста")
     else:
         form = AddPostForm()
 
     data = {
         'menu': menu,
-        'title': 'Добавление страницы',
+        'title': 'Добавление статьи',
         'form': form
     }
     return render(request, 'women/addpage.html', data)
