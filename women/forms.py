1a8e81d274aaa5de5ffc0322ac9d536e4dcdb3a6
nvLast86 <n.lastenko@gmail.com>
2024-07-29 15:11:50 +0600

    task 7.5 has been completed

--- women/forms.py
+++ women/forms.py
@@ -2,29 +2,29 @@ from django import forms
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.core.exceptions import ValidationError
 
-from .models import Category, Husband
+from .models import Category, Husband, Women
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Заголовок", min_length=5,
-                            error_messages={
-                                'min_length': 'Слишком короткое название',
-                                'required': 'Без заголовка никак',
-                            })
-    slug = forms.SlugField(max_length=255, label="URL",
-                           validators=[
-                               MinLengthValidator(5, message='Минимум 5 символов'),
-                               MaxLengthValidator(255, message='Максимум 255 символов'),
-                           ])
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
-    is_published = forms.BooleanField(required=False, initial=True, label='Статус')
+class AddPostForm(forms.ModelForm):
+
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категории')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, empty_label='Не замужем', label='Муж')
 
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'husband', 'tags']
+        widget = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 50, 'rows': 5})
+        }
+        label = {'slug': 'URL'}
+
     def clean_title(self):
         title = self.cleaned_data['title']
-        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмпнопрстуфхцчшщьыъэюя0123456789- "
+        if len('title') > 50:
+            raise ValidationError('Длина превышает 50 символов')
 
-        if not (set(title) <= set(ALLOWED_CHARS)):
-            raise ValidationError("Должны присутствовать только русские символы, дефис и пробел")
+        return title
+
+
 
