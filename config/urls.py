90a1333c144f433edc26f8e87a0b490af6eebba7
nvLast86 <n.lastenko@gmail.com>
2024-07-30 16:20:36 +0600

    task 7.8 has been completed

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
+from config import settings
 from women import views
 from women.views import page_not_found
 
@@ -25,6 +27,9 @@ urlpatterns = [
     path('', include('women.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
 
--- women/admin.py
+++ women/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
 
 from women.models import Women, Category
 
@@ -23,12 +24,12 @@ class MarriedFilter(admin.SimpleListFilter):
 # Register your models here.
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    fields = ['title', 'slug', 'content', 'cat', 'husband', 'tags']
+    fields = ['title', 'slug', 'content', 'photo', 'post_photo', 'cat', 'husband', 'tags']
     # exclude = ('tags', 'is_published')
-    # readonly_fields = ('slug',)
+    readonly_fields = ('post_photo',)
     prepopulated_fields = {'slug': ('title', )}
     filter_horizontal = ['tags']
-    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display = ('title', 'post_photo', 'time_create', 'is_published', 'cat',)
     list_display_links = ('title',)
     ordering = ('time_create', 'title')
     list_editable = ('is_published', )
@@ -36,10 +37,13 @@ class WomenAdmin(admin.ModelAdmin):
     actions = ['set_published', 'set_draft']
     search_fields = ['title__startswith', 'cat__name']
     list_filter = [MarriedFilter, 'cat__name', 'is_published']
+    save_on_top = True
 
     @admin.display(description='Краткое описание', ordering='content')
-    def brief_info(self, women: Women):
-        return f'Описание {len(women.content)} символов.'
+    def post_photo(self, women: Women):
+        if women.photo:
+            return mark_safe(f"<img src='{women.photo.url} width=50">)
+        return 'Без фото'
 
     @admin.action(description='Опубликовать выбранные записи')
     def set_published(self, request, queryset):
