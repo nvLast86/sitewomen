3d9317318c53e70c080b02d34472f0cd139837f2
nvLast86 <n.lastenko@gmail.com>
2024-08-15 13:26:58 +0600

    task 9.4 has been completed

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(max_length=50, label='Логин',
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(max_length=50, label='Пароль',
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password',]
+
+
