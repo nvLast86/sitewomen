353fdcff1d4fc685b4290f5a33a4f8ca430b1b0d
nvLast86 <n.lastenko@gmail.com>
2024-08-18 18:16:15 +0600

    task 9.10 has been completed

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
