6bf3adfe46ef823d3114410812b453ddaa6ac696
nvLast86 <n.lastenko@gmail.com>
2024-08-30 12:53:08 +0600

    task 9.14 has been completed

--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from datetime import datetime
@@ -80,10 +80,11 @@ class TagPostList(DataMixin, ListView):
         return self.get_mixin_context(context, title='Тег: ' + tag.tag)
 
 
-class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+class AddPage(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
     title_page = 'Добавление статьи'
+    permission_required = 'women.add_women'
 
     def form_valid(self, form):
         w = form.save(commit=False)
@@ -91,19 +92,20 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         return super().form_valid(form)
 
 
-class UpdatePage(UpdateView):
+class UpdatePage(PermissionRequiredMixin, UpdateView):
     model = Women
     fields = ['title', 'content', 'photo', 'is_published', 'cat']
     template_name = 'women/addpage.html'
     title_page = 'Редактирование статьи'
     success_url = reverse_lazy('home')
+    permission_required = 'women.change_women'
 
 
 class DeletePage(DeleteView):
     model = Women
     success_url = reverse_lazy('home')
 
-
+@permission_required(perm='women.view_women', raise_exception=True)
 def contact(request):
     return HttpResponse("Обратная связь")
 
