f1e4b32694feba62fb53a13c636bd245d84ca8ca
nvLast86 <n.lastenko@gmail.com>
2024-08-30 14:16:00 +0600

    task 10.2 has been completed

--- config/settings.py
+++ config/settings.py
@@ -165,3 +165,19 @@ AUTH_USER_MODEL = 'users.User'
 SOCIAL_AUTH_JSONFIELD_ENABLED = True
 
 DEFAULT_USER_IMAGE = MEDIA_URL + "users/default.png"
+
+SOCIAL_AUTH_GITHUB_KEY = 'Ov23liVLDMkPKIJo6hUo'
+SOCIAL_AUTH_GITHUB_SECRET = 'cc1fbf5588b78929cdc41b87a7500e51bd988de4'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.new_users_handler',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
